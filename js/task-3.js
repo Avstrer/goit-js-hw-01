cf0f00b7de43e563b0f9caf2ca331ba7b7c01f6f
Avstrer <148279347+Avstrer@users.noreply.github.com>
2024-01-17 15:17:23 +0200

    complete

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,8 @@
 function getElementWidth(content, padding, border) {
-  Number.parseInt(content);
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) +
-    Number.parseFloat(padding) +
-    Number.parseFloat(border) +
-    Number.parseFloat(border)
-  );
+  const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding);
+  const borderWidth = Number.parseFloat(border);
+  return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
